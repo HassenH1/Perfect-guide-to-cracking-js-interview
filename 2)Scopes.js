df9e7ceab8ff109df98b6e333d723f7bd9cb7ccd
HassenH1 <hasansaid51@gmail.com>
2020-06-11 16:20:15 -0700

    closure

--- 2)Scopes.js
+++ 2)Scopes.js
@@ -22,3 +22,37 @@ console.log(
   pi,
   "<-------this works because pi isnt a 'var' or 'let' so its globally changable"
 ); //3.14159
+
+// Block scope(Introduced in ES6)
+var a = 10;
+
+function Foo() {
+  if (true) {
+    let a = 4;
+    console.log(a, "<-------what this?"); //is is 4 within the scope of the conditional
+  }
+
+  // alert(a); // alerts '10' because the 'let' keyword
+  console.log(a); // 10
+}
+Foo();
+
+//JavaScript closure is a function that returns another function.
+function generator(input) {
+  var index = 0;
+  return {
+    next: function () {
+      if (index < input.length) {
+        index += 1;
+        return input[index - 1];
+      }
+      return "";
+    },
+  };
+}
+
+var mygenerator = generator("boomerang");
+mygenerator.next(); // returns "b"
+mygenerator.next(); // returns "o"
+mygenerator = generator("toon");
+mygenerator.next(); // returns "t"
