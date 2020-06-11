8a01adac2291c54474816623172cf9d0717eb904
HassenH1 <hasansaid51@gmail.com>
2020-06-11 14:58:42 -0700

    function scope

--- 2)Scopes.js
+++ 2)Scopes.js
@@ -9,3 +9,16 @@ function Foo() {
   console.log(x); // Prints 10
 }
 Foo();
+
+//Local/Function scope
+pi = 3.14;
+function circumference(radius) {
+  console.log(pi); //prints 3.14
+  pi = 3.14159; //gets reassigned here
+  console.log(2 * pi * radius); // Prints "12.56636" not "12.56"
+}
+circumference(2);
+console.log(
+  pi,
+  "<-------this works because pi isnt a 'var' or 'let' so its globally changable"
+); //3.14159
