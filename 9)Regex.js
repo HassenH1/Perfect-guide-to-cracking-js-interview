3966608e1b9921a478bebe1d62812b3641736569
HassenH1 <hasansaid51@gmail.com>
2020-06-12 21:42:06 -0700

    regex elements

---
+++ 9)Regex.js
@@ -0,0 +1,9 @@
+// We can create a new regular expression using this
+var re = /ar/;
+var re = new RegExp("ar"); // This too works
+
+// There are many types of elements in RegEx. Some of them are:
+// Characters Ex: \w — Alphanumeric, \d — Decimal, \D — Non decimal
+// Character classes Ex: [x-y] in range x to y, [^x] not x
+// Quantifiers Ex: +, ?, * (greedy and lazy matchers)
+// Boundaries Ex: ^(beginning of input), $(end of input)
