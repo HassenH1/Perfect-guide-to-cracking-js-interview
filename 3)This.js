740f4e4dd94de652ff0c0c50cb9696b0115ed9ce
HassenH1 <hasansaid51@gmail.com>
2020-06-12 22:41:09 -0700

    .call() method

--- 3)This.js
+++ 3)This.js
@@ -1,5 +1,5 @@
-// If you take the browser, in the global context it refers to the window object
-// this === window;
+// If you take the browser, in the global context "this" refers to the window object
+// this === window; //true
 
 //When the context and scope of program changes, this at that particular point changes accordingly. Now see this in a local context is:
 
@@ -7,7 +7,9 @@ function Foo() {
   console.log(this.a, "<---------------------------is this it?");
 }
 var food = { a: "Magical this" };
-Foo(food); // food is this
+Foo.call(food); // food is this
+// The ^^call() allows for a function/method belonging to one object to be assigned and called for a different object.
+// call() provides a new value of this to the function/method. With call(), you can write a method once and then inherit it in another object, without having to rewrite the method for the new object
 
 function Foo() {
   console.log(this, "<-------------------------is this it?"); //this is a global object here and would return the window object
