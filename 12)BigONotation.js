3d17850a752bbd01c600d8bab0fb7fddb30a6b6b
HassenH1 <hasansaid51@gmail.com>
2020-06-24 14:38:57 -0700

    Log

--- 12)BigONotation.js
+++ 12)BigONotation.js
@@ -31,3 +31,22 @@ function addAndLog(array) {
     }
   }
 }
+
+//Logarithmic => O(log n)
+function binarySearch(array, key) {
+  var low = 0;
+  var high = array.length - 1;
+  var mid;
+  var element;
+
+  while (low <= high) {
+    mid = Math.floor((low + high) / 2, 10);
+    element = array[mid];
+    if (element < key) {
+      high = mid = 1;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+}
