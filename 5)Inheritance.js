ec251095545d8f5b8a96e0383322d1b76169e82d
HassenH1 <hasansaid51@gmail.com>
2020-07-16 23:52:09 -0700

    got it

--- 5)Inheritance.js
+++ 5)Inheritance.js
@@ -5,9 +5,17 @@ var X = function (j) {
   this.i = 0;
   this.j = j;
 
-  this.getJ = function () {
-    return this.j;
-  };
+  // this.getJ = function () {
+  //   return this.j;
+  // };
+};
+
+//^^^^this is how you can add methods and properties to function constructor^^^^
+//every function has a property called prototype
+//now we are attaching this method to itVV
+X.prototype.getJ = function () {
+  //{objectName}.prototype.{methodName}
+  return this.j;
 };
 
 var x1 = new X(1);
