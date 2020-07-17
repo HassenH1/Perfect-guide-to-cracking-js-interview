d7f06c7a49c40a8b3f9693c4f9c14df5451751cb
HassenH1 <hasansaid51@gmail.com>
2020-07-16 23:46:35 -0700

    added a good example

--- 5)Inheritance.js
+++ 5)Inheritance.js
@@ -1,1 +1,17 @@
 // All the new class syntax you see in ES5, ES6 is just a syntactical sugar coating for the underlying prototypical OOP. Creating a class is done using a function in JavaScript.
+
+var X = function (j) {
+  //<--good practice to capitalize function constructor
+  this.i = 0;
+  this.j = j;
+
+  this.getJ = function () {
+    return this.j;
+  };
+};
+
+var x1 = new X(1);
+var x2 = new X(2);
+
+console.log(x1.getJ());
+console.log(x2.getJ());
