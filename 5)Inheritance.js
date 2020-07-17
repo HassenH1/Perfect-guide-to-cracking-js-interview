fc72aa75860268e883e95a2a047c7ccef7682247
HassenH1 <hasansaid51@gmail.com>
2020-07-17 00:07:39 -0700

    finish for now but there is more to learn

--- 5)Inheritance.js
+++ 5)Inheritance.js
@@ -1,6 +1,7 @@
 // All the new class syntax you see in ES5, ES6 is just a syntactical sugar coating for the underlying prototypical OOP. Creating a class is done using a function in JavaScript.
 
-var X = function (j) {
+//function constructor --------------------
+var Y = function (j) {
   //<--good practice to capitalize function constructor
   this.i = 0;
   this.j = j;
@@ -13,13 +14,31 @@ var X = function (j) {
 //^^^^this is how you can add methods and properties to function constructor^^^^
 //every function has a property called prototype
 //now we are attaching this method to itVV
-X.prototype.getJ = function () {
+Y.prototype.getJ = function () {
   //{objectName}.prototype.{methodName}
   return this.j;
 };
 
-var x1 = new X(1);
-var x2 = new X(2);
+var x1 = new Y(1);
+var x2 = new Y(2);
 
 console.log(x1.getJ());
 console.log(x2.getJ());
+
+//---------------------------
+
+//Master Object -------------------
+
+var X = function () {};
+
+console.dir(X, "<-------what this"); //allows you to look at the object in more detail
+/*
+print this ous
+function x()
+  arguments: null
+  caller: null
+  length: 0
+  name: ""
+prototype: x
+__proto__: function() //if you expand this it has a bunch of methods and properties and X is ultimately created from this Object
+*/
